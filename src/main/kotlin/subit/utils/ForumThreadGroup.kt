10331db739a5adc782807e3ec4b7473c6e3a9bd9
nullaqua <nullaqua@outlook.com>
2024-06-14 12:48:18 +0800

    fix: 修复ktor启动失败时stop命令无法正常工作的问题

--- src/main/kotlin/subit/utils/ForumThreadGroup.kt
+++ src/main/kotlin/subit/utils/ForumThreadGroup.kt
@@ -18,20 +18,24 @@ object ForumThreadGroup: ThreadGroup("ForumThreadGroup"), KoinComponent
 
     fun shutdown(code: Int, cause: String = "unknown"): Nothing
     {
-        val application = getKoin().get<Application>()
+        val application = runCatching { getKoin().get<Application>() }.getOrNull()
         application.shutdown(code, cause)
     }
-    fun Application.shutdown(code: Int, cause: String = "unknown"): Nothing
+    fun Application?.shutdown(code: Int, cause: String = "unknown"): Nothing
     {
         logger.warning("${PURPLE}Server is shutting down: ${CYAN}$cause${RESET}")
         // 尝试主动结束Ktor, 这一过程不一定成功, 例如Ktor本来就在启动过程中出错将关闭失败
-        runCatching()
+        if (this != null) runCatching()
         {
             val environment = this.environment
             environment.monitor.raise(ApplicationStopPreparing, environment)
             if (environment is ApplicationEngineEnvironment) environment.stop()
             else this@shutdown.dispose()
+        }.onFailure {
+            logger.warning("Failed to stop Ktor: ${it.message}")
+            it.printStackTrace(ForumLogger.err)
         }
+        else logger.warning("Application is null")
         // 无论是否成功关闭, 都强制退出
         exitProcess(code)
     }
